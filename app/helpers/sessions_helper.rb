4b0255a80d410a71a82310f318f2dc6f8856dabf
s3731026@student.rmit.edu.au <siddharthraju123@gmail.com>
2020-05-15 08:10:18 +0000

    USer

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,21 +4,25 @@ module SessionsHelper
     end
 
     def remember(user)
-        user.remember
+        user.remember_me
         cookies.permanent.signed[:user_id]=user.id
         cookies.permanent[:remember_token]=user.remember_token
+    end
+
+    def current_user?(user)
+        user==current_user
     end
 
 
     def current_user
         if (user_id=session[:user_id])
             @current_user ||=User.find_by(id: user_id)
-        elsif (user_id=cookies.signed[:user_id])
+        elsif (user_id = cookies.signed[:user_id])
            # raise
             user=User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
                 log_in user 
-                @current_user=user
+                @current_user = user
             end 
         end
     end
@@ -27,9 +31,7 @@ module SessionsHelper
         !current_user.nil?
     end
 
-    def current_user?(user)
-        user==current_user
-    end
+    
 
     def forget(user)
         user.forget
@@ -40,11 +42,11 @@ module SessionsHelper
     def log_out
         forget(current_user)
         session.delete(:user_id)
-        @current_user=nil
+        @current_user = nil
     end
 
     def redirect_back_or(default) 
-        redirect_to(session[:forwarding_url]||default)
+        redirect_to(session[:forwarding_url] || default)
         session.delete(:forwarding_url)
     end
     # Stores the URL trying to be accessed. 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor:remember_token
+    attr_accessor :remember_token
     validates:name,presence:true,length: {maximum:50}
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -17,13 +17,19 @@ class User < ApplicationRecord
     end
 
     def authenticated?(remember_token)
-        returnfalseifremember_digest.nil?
+        return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 
     def forget
         update_attribute(:remember_digest,nil)
     end
+
+    def remember
+        self.remember_token=User.new_token
+        update_attribute(:remember_digest,User.digest(remember_token))
+    end
+
 
     
 end
